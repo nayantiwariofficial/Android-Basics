56b6163daf74a0ef530109c43b73756f25ae056e
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-22 04:05:00 +0530

    Just Java 2 app update
    Just Java 2 app update

--- JustJava2/app/src/main/java/com/example/nayantiwari/justjava2/MainActivity.java
+++ JustJava2/app/src/main/java/com/example/nayantiwari/justjava2/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.nayantiwari.justjava2;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import static com.example.nayantiwari.justjava2.R.id.checkBox1;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkBox1, checkBox2;
     boolean checkBoxValue1, checkBoxValue2;
     private TextView qty;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +32,67 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrders(View view) {
-        displayMessage(createOrderSummary(calculatePrice()));
+
+//        displayMessage(createOrderSummary(calculatePrice()));
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, "nayan.tiwari@asu.edu");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.orderSum));
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary(calculatePrice()));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private String createOrderSummary(int price) {
         String text = qty.getText().toString();
-        return "Name: Nayan Tiwari" +
-                "\nAdd Whipped Cream? " + checkBoxValue1 +
-                "\nAdd Chocolate? " + checkBoxValue2 +
-                "\nQuantity: " + text +
-                "\nTotal: $" + price + "\n" +
-                "Thank you!";
+        if(numberOfCoffees>0) {
+            String name;
+            EditText editText = (EditText) findViewById(R.id.editText);
+            name = editText.getText().toString();
+            return getString(R.string.name_s, name) +
+                    getString(R.string.addWhippedCream) + checkBoxValue1 +
+                    getString(R.string.addChocolate) + checkBoxValue2 +
+                    getString(R.string.quan) + text +
+                    getString(R.string.tot) + price + "\n" +
+                    getString(R.string.thank_you);
+        }
+        else{
+            Toast.makeText(this, R.string.cannotOrderZeroNumberOfCoffees, Toast.LENGTH_SHORT).show();
+            return text;
+        }
+
     }
 
     private int calculatePrice() {
-        return numberOfCoffees * 5;
+        if (checkBoxValue1) {
+            if (checkBoxValue2) {
+                Log.i(TAG, "calculatePrice: Both");
+                return numberOfCoffees * (5 + 3);
+            } else {
+                Log.i(TAG, "calculatePrice: Only 1");
+                return numberOfCoffees * (5 + 1);
+            }
+        } else {
+            if (checkBoxValue2) {
+                Log.i(TAG, "calculatePrice: Only 2");
+                return numberOfCoffees * (5 + 2);
+            } else {
+                Log.i(TAG, "calculatePrice: None");
+                return numberOfCoffees * 5;
+            }
+        }
+
     }
 
     public void addNumberOfCoffees(View view) {
-        numberOfCoffees++;
-        displayQuantity(numberOfCoffees);
+        if(numberOfCoffees <= 100) {
+            numberOfCoffees++;
+            displayQuantity(numberOfCoffees);
+        }
+        else{
+            Toast.makeText(this, R.string.cupLimit100, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void removeNumberOfCoffees(View view) {
@@ -57,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             displayQuantity(numberOfCoffees);
         } else {
             numberOfCoffees = 0;
+            Toast.makeText(this, R.string.cupNumNoNeg, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -72,16 +117,12 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
+
 
     public void changeCheckedValue(View view) {
         checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
         checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
         checkBoxValue1 = checkBox1.isChecked();
         checkBoxValue2 = checkBox2.isChecked();
-
     }
 }
