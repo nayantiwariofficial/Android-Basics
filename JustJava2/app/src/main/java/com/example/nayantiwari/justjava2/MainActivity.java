baaf6afe1557619ab195b55ae5149286525ad1e8
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-21 08:16:22 +0530

    Added more stuff
    Added checkboxes

--- JustJava2/app/src/main/java/com/example/nayantiwari/justjava2/MainActivity.java
+++ JustJava2/app/src/main/java/com/example/nayantiwari/justjava2/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.nayantiwari.justjava2;
 
 import android.os.Bundle;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import static com.example.nayantiwari.justjava2.R.id.checkBox1;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
     private int numberOfCoffees = 0;
+    private CheckBox checkBox1, checkBox2;
+    boolean checkBoxValue1, checkBoxValue2;
     private TextView qty;
 
     @Override
@@ -28,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
 
     private String createOrderSummary(int price) {
         String text = qty.getText().toString();
-        return getString(R.string.template1, "Nayan", text, price);
-//        return "Name: Nayan Tiwari\nQuantity: " + text + "\nTotal: $" + price + "\n" +
-//                "Thank you!";
+        return "Name: Nayan Tiwari" +
+                "\nAdd Whipped Cream? " + checkBoxValue1 +
+                "\nAdd Chocolate? " + checkBoxValue2 +
+                "\nQuantity: " + text +
+                "\nTotal: $" + price + "\n" +
+                "Thank you!";
     }
 
     private int calculatePrice() {
@@ -66,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
+    }
+
+    public void changeCheckedValue(View view) {
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBoxValue1 = checkBox1.isChecked();
+        checkBoxValue2 = checkBox2.isChecked();
+
     }
 }
