44daaab3db450741e4fdccdef87daab8e29eb9f6
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-22 17:43:02 +0530

    Quiz App Updated
    Quiz App Updated

--- QuizApp/app/src/main/java/com/example/nayantiwari/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/nayantiwari/quizapp/MainActivity.java
@@ -8,16 +8,6 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import static com.example.nayantiwari.quizapp.R.id.checkBox1;
-import static com.example.nayantiwari.quizapp.R.id.checkBox2;
-import static com.example.nayantiwari.quizapp.R.id.checkBox3;
-import static com.example.nayantiwari.quizapp.R.id.checkBox4;
-import static com.example.nayantiwari.quizapp.R.id.checkBox5;
-import static com.example.nayantiwari.quizapp.R.id.checkBox6;
-import static com.example.nayantiwari.quizapp.R.id.checkBox7;
-import static com.example.nayantiwari.quizapp.R.id.checkBox8;
-import static com.example.nayantiwari.quizapp.R.id.editText1;
-
 public class MainActivity extends AppCompatActivity {
 
     boolean question1 = false;
@@ -34,16 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkAnswers(View view) {
 
+        int score = 0;
+        String incorrectQuestion = "";
         checkQuestion1();
         checkQuestion2();
         checkQuestion3();
         checkQuestion4();
         checkQuestion5();
 
-        if(question1 && question2 && question3 && question4 && question5)
-        {
-            Toast.makeText(this, "All your answers are correct", Toast.LENGTH_SHORT).show();
-        }
+        if (question1)
+            score++;
+        else incorrectQuestion += "1";
+        if (question2)
+            score++;
+        else incorrectQuestion += " 2";
+        if (question3)
+            score++;
+        else incorrectQuestion += " 3";
+        if (question4)
+            score++;
+        else incorrectQuestion += " 4";
+        if (question5)
+            score++;
+        else incorrectQuestion += " 5";
+
+        Toast.makeText(this, "Your score " + score + "/5", Toast.LENGTH_SHORT).show();
+
+        if((!(question1 && question2 && question3 && question4 && question5)))
+            Toast.makeText(this, "Incorrect Questions are: " + incorrectQuestion, Toast.LENGTH_SHORT).show();
     }
 
     private void checkQuestion1() {
@@ -55,11 +63,13 @@ public class MainActivity extends AppCompatActivity {
         if (checkBox2.isChecked() && !checkBox1.isChecked() && !checkBox3.isChecked()) {
             if (checkBox4.isChecked()) {
                 question1 = true;
-                Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
-            } else
-                Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
-        } else
-            Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
+            }
+//            else
+//                Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
+        }
+//        else
+//            Toast.makeText(this, "Question 1 is " + question1, Toast.LENGTH_SHORT).show();
 
     }
 
@@ -72,11 +82,13 @@ public class MainActivity extends AppCompatActivity {
         if (checkBox5.isChecked() && !checkBox7.isChecked() && !checkBox8.isChecked()) {
             if (checkBox6.isChecked()) {
                 question2 = true;
-                Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
-            } else
-                Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
-        } else
-            Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
+            }
+            //else
+//                Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
+        }
+// else
+//            Toast.makeText(this, "Question 2 is " + question2, Toast.LENGTH_SHORT).show();
     }
 
     private void checkQuestion3() {
@@ -85,24 +97,29 @@ public class MainActivity extends AppCompatActivity {
 
         if (radioButton1.isChecked()) {
             question3 = true;
-            Toast.makeText(this, "Question 3 is " + question3, Toast.LENGTH_SHORT).show();
-        } else
-            Toast.makeText(this, "Question 3 is " + question3, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "Question 3 is " + question3, Toast.LENGTH_SHORT).show();
+        }
+//        else
+//            Toast.makeText(this, "Question 3 is " + question3, Toast.LENGTH_SHORT).show();
     }
 
     private void checkQuestion4() {
         EditText editText1 = (EditText) findViewById(R.id.editText1);
-        if(editText1.getText().toString().equalsIgnoreCase("George Washington")){
+        if (editText1.getText().toString().equalsIgnoreCase("George Washington")) {
             question4 = true;
-            Toast.makeText(this, "Question 4 is " + question4, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "Question 4 is " + question4, Toast.LENGTH_SHORT).show();
         }
+//        else
+//            Toast.makeText(this, "Question 4 is " + question4, Toast.LENGTH_SHORT).show();
     }
 
     private void checkQuestion5() {
         EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if(editText2.getText().toString().equalsIgnoreCase("Arizona State University") || editText2.getText().toString().equalsIgnoreCase("ASU")){
+        if (editText2.getText().toString().equalsIgnoreCase("Arizona State University") || editText2.getText().toString().equalsIgnoreCase("ASU")) {
             question5 = true;
-            Toast.makeText(this, "Question 5 is " + question5, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "Question 5 is " + question5, Toast.LENGTH_SHORT).show();
         }
+//        else
+//            Toast.makeText(this, "Question 5 is " + question4, Toast.LENGTH_SHORT).show();
     }
 }
