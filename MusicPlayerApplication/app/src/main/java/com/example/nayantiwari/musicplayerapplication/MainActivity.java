694e1436b4700445c10b6c90cf78fdddb3767870
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-26 20:01:50 +0530

    Music Demo App
    Music Demo App

---
+++ MusicPlayerApplication/app/src/main/java/com/example/nayantiwari/musicplayerapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.nayantiwari.musicplayerapplication;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.phrase_im_feeling_good);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+        Button unmute = (Button) findViewById(R.id.unmute);
+        unmute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.setVolume(100, 100);
+            }
+        });
+
+        Button mute = (Button) findViewById(R.id.mute);
+        mute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.setVolume(0, 0);
+            }
+        });
+    }
+}
