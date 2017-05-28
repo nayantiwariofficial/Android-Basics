3d88a4063ad5cb6889b33df6c50597775feb8a3a
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-29 01:52:34 +0530

    View pager Udacity demo
    View pager Udacity demo

---
+++ ud839_ViewPager_Example-quiz/app/src/main/java/com/example/android/viewpager/ThursdayFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.viewpager;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nayantiwari on 5/29/17.
+ */
+
+public class ThursdayFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_thursday, container, false);
+    }
+}
