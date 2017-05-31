d26ec5f02ba8b9bdbf50e3443d6638dde8db2842
Nayan Tiwari <nayan.tiwari@asu.edu>
2017-05-31 05:46:55 +0530

    Networking tutorial Udacity
    Networking tutorial Udacity

--- ud843_Soonami-starting-point/app/src/main/java/com/example/android/soonami/MainActivity.java
+++ ud843_Soonami-starting-point/app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -154,6 +154,10 @@ public class MainActivity extends AppCompatActivity {
          */
         private String makeHttpRequest(URL url) throws IOException {
             String jsonResponse = "";
+            if(url == null)
+            {
+                return jsonResponse;
+            }
             HttpURLConnection urlConnection = null;
             InputStream inputStream = null;
             try {
@@ -162,10 +166,14 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setReadTimeout(10000 /* milliseconds */);
                 urlConnection.setConnectTimeout(15000 /* milliseconds */);
                 urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+                if(urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                } else
+                    Log.e(LOG_TAG, "Error response code" + urlConnection.getResponseCode());
+
             } catch (IOException e) {
-                // TODO: Handle the exception
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results" + e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
